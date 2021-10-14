c0342f6243be01bae6ac8a83164efe5c4a9fbf13
kennammoja <91263362+kennammoja@users.noreply.github.com>
2021-10-14 13:30:09 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/kwsvisitorregistrationsystem/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.kwsvisitorregistrationsystem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    confirmLogin.setOnClickListener {
+
+        if (loginCred.text.trim().isNotEmpty() || password.text.trim().isNotEmpty)
+
+
+
+    }
+}
