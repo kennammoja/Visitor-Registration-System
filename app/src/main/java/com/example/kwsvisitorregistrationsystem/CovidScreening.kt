2b5025f5707554e3adb9b693bf60c2ed047da1ed
Eric Wamari <ericwamari@gmail.com>
2021-10-14 14:20:11 +0300

    proposed activities

---
+++ app/src/main/java/com/example/kwsvisitorregistrationsystem/CovidScreening.kt
@@ -0,0 +1,11 @@
+package com.example.kwsvisitorregistrationsystem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CovidScreening : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_covid_screening)
+    }
+}
